3977b607b3ba0fa782a51de8fd54f088b1ba5ecf
pansport <111249835+pansport@users.noreply.github.com>
2022-10-28 12:45:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
+// modal window
+///////////////////////////////////////
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -32,6 +33,7 @@ document.addEventListener('keydown', function (e) {
 
 ///////////////////////////////////////
 // smooth scrolling
+///////////////////////////////////////
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
@@ -42,6 +44,7 @@ btnScrollTo.addEventListener('click', () => {
 
 ///////////////////////////////////////
 // page navigation
+///////////////////////////////////////
 
 // 1
 // document.querySelectorAll('.nav__link').forEach(function (link) {
@@ -63,6 +66,7 @@ document.querySelector('.nav__links').addEventListener('click', event => {
 
 ///////////////////////////////////////
 // tabbed component
+///////////////////////////////////////
 
 const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
@@ -85,6 +89,82 @@ tabsContainer.addEventListener('click', event => {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+///////////////////////////////////////
+// menu fade animation
+///////////////////////////////////////
+
+const nav = document.querySelector('.nav');
+
+const handleHover = function (event) {
+  if (event.target.classList.contains('nav__link')) {
+    const link = event.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+
+    siblings.forEach(element => {
+      if (element !== link) element.style.opacity = this;
+    });
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////////////////
+// sticky navigation
+///////////////////////////////////////
+
+// const initialCoords = section1.getBoundingClientRect();
+
+// window.addEventListener('scroll', () => {
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+///////////////////////////////////////
+// reveal sections
+///////////////////////////////////////
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(section => {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 //////////////////////////////////////////////
